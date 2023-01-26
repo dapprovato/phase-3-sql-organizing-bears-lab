1cfb4d4ede41ca6a6a3ad5ab885b488fbfcf9770
Daria Approvato <dariaapprovato@gmail.com>
2023-01-25 20:59:49 -0500

    commit tree 8ea1b7d22935fcfac674f4705c71628b742eded9

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'brown', 'sweet', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M', 'pink', 'salty', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'T', 'brown', 'sweet', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'brown', 'salty', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'pink', 'sweet', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'green', 'salty', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'black', 'sweet', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 2, 'M', 'brown', 'sweet', true);
